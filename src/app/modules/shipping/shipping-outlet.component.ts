175a353d74aab51440f9a05d4961ee317feaf936
Igor Joshevski <gigo_j44@hotmail.com>
2020-09-15 18:47:35 +0200

    Create shipping module & router

---
+++ src/app/modules/shipping/shipping-outlet.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shipping-outlet',
+  templateUrl: './shipping-outlet.component.html',
+  styleUrls: ['./shipping-outlet.component.css']
+})
+export class ShippingOutletComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
