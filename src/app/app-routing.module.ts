8845c0dc73f6447a330760c3da0ec22bbc538401
unknown <todorov.mirko@gmail.com>
2020-09-15 19:18:15 +0200

    Users Module basic implementation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,27 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
 const routes: Routes = [
   //{path: 'dashboard', loadChildren: () => {}},
   //{path: 'products', loadChildren: () => {}},
   //{path: 'login', component: ''},
   //{path: 'register', component: ''},
-  { path: 'shipping', loadChildren: () => import('./modules/shipping/shipping.module').then(shipping => shipping.ShippingModule) },
- 
+  {
+    path: "shipping",
+    loadChildren: () =>
+      import("./modules/shipping/shipping.module").then(
+        (shipping) => shipping.ShippingModule
+      ),
+  },
+  {
+    path: "users",
+    loadChildren: () =>
+      import("./modules/users/users.module").then((users) => users.UsersModule),
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
