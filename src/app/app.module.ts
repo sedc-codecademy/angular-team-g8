3b3f13eb99242b3f29576b2a7af3dcaa9d8e2907
unknown <todorov.mirko@gmail.com>
2020-09-15 20:11:54 +0200

    Commiting App Shell

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from "./app-routing.module";
 import { Router } from "@angular/router";
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./components/header/header.component";
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
