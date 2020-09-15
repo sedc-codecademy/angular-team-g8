408ff548e2100d55186a66a926d94c5e06cb94f4
tpetrovska <tpetrovskaa@gmail.com>
2020-09-15 19:49:28 +0200

    products model

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
 const routes: Routes = [
-  //{path: 'dashboard', loadChildren: () => {}},
-  //{path: 'products', loadChildren: () => {}},
-  //{path: 'login', component: ''},
-  //{path: 'register', component: ''},
+  // {path: 'dashboard', loadChildren: () => {}},
+  // {path: 'products', loadChildren: () => {}},
+  // {path: 'login', component: ''},
+  // {path: 'register', component: ''},
+  {
+    path: 'products',
+    loadChildren: () =>
+      import('./modules/products/products.module').then(
+        (products) => products.ProductsModule
+      ),
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
