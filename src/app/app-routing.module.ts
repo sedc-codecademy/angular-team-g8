aad3e59af0b018c03aa8642c98340e9ab78c2049
AlamoFiteCat <shredgod_lobos@hotmail.com>
2020-09-15 19:43:31 +0200

    login page UI done, connected to router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
+import { LoginComponent } from "./components/login/login.component";
 
 const routes: Routes = [
   //{path: 'dashboard', loadChildren: () => {}},
   //{path: 'products', loadChildren: () => {}},
-  //{path: 'login', component: ''},
+  { path: "login", component: LoginComponent },
   //{path: 'register', component: ''},
   {
     path: "shipping",
