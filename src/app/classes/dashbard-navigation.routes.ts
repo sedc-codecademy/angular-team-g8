519f1ed9324edaf6ddc66cba9ffabd389e83f2a5
Igor Joshevski <gigo_j44@hotmail.com>
2020-09-17 20:09:13 +0200

    Update of CSS

--- src/app/classes/dashbard-navigation.routes.ts
+++ src/app/classes/dashbard-navigation.routes.ts
@@ -3,10 +3,10 @@ export class DashboardRoutes {
   routes(): Array<object> {
     return [
       { name: 'Users', url: 'users' },
-      { name: 'Products', url: 'orders' },
-      { name: 'Orders', url: 'invoices' },
-      { name: 'Invoices', url: 'shipping' },
-      { name: 'Shipping', url: 'products' },
+      { name: 'Products', url: 'products' },
+      { name: 'Orders', url: 'orders' },
+      { name: 'Invoices', url: 'invoices' },
+      { name: 'Shipping', url: 'shipping' },
     ];
   }
 
